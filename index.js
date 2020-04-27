6bf3c9ef7f13ab9794ec1dacbac353928a945ec3
dwinniford <dwinniford@gmail.com>
2020-04-27 14:36:16 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(recordArray) {
+  if (!!recordArray.find( function(r) { return  r.result === 'W'} )) {
+    return recordArray.find( function(r) { return  r.result === 'W'} ).year
+  } else {
+    return undefined 
+  }
+ 
+} 
